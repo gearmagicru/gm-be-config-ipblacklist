70754b1313fdcd2b1ef16060978c0bd6497be5e2
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-17 15:52:59 +0300

    Файлы конфигурации

---
+++ config/version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации версии расширения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'name'        => 'IP Blacklist',
+    'description' => 'Prevent users from accessing the control panel or site from specified IP addresses',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'GearMagic',
+    'authorUrl'   => 'https://gearmagic.ru',
+    'email'       => 'anton.tivonenko@gmail.com',
+    'url'         => 'https://apps.gearmagic.ru/component/gm-be-config-ipblacklist',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://apps.gearmagic.ru/license/mit-license.html'
+];
